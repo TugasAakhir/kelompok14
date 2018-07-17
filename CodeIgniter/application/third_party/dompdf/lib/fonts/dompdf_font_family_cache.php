4f68c28b6163e59bdaa13204e4736764e6b9300f
arrizky <arrizkyrama00@gmail.com>
2018-07-18 06:07:50 +0700

    no message

---
+++ CodeIgniter/application/third_party/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,113 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '\ZapfDingbats',
+    'bold' => $fontDir . '\ZapfDingbats',
+    'italic' => $fontDir . '\ZapfDingbats',
+    'bold_italic' => $fontDir . '\ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '\Symbol',
+    'bold' => $fontDir . '\Symbol',
+    'italic' => $fontDir . '\Symbol',
+    'bold_italic' => $fontDir . '\Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '\DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSans-Oblique',
+    'normal' => $fontDir . '\DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '\DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '\DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '\DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '\DejaVuSerif-Italic',
+    'normal' => $fontDir . '\DejaVuSerif',
+  ),
+  'fontawesome' => array(
+    'normal' => $fontDir . '\b81f8d4cdb4bef85c8477375b63bd956',
+  ),
+  'simple-line-icons' => array(
+    'normal' => $fontDir . '\7a9561ed7a071febfb37d1fab63404cf',
+  ),
+  'linea-arrows-10' => array(
+    'normal' => $fontDir . '\cd8764f8cb577c46053de2c2f6c6d813',
+  ),
+  'linea-basic-10' => array(
+    'normal' => $fontDir . '\81a98c60a9f91f11cde46f712abfa7e0',
+  ),
+  'linea-basic-elaboration-10' => array(
+    'normal' => $fontDir . '\938d1fae8aedbb96cfacf9a38713c617',
+  ),
+  'linea-ecommerce-10' => array(
+    'normal' => $fontDir . '\9572f20405315d1da82b2a47d22f7fe6',
+  ),
+  'linea-music-10' => array(
+    'normal' => $fontDir . '\59bcb3f5a875e1e820477d235bddcb10',
+  ),
+  'linea-software-10' => array(
+    'normal' => $fontDir . '\3bd9e49e9d9afe17bd2fdd494b9ec093',
+  ),
+  'linea-weather-10' => array(
+    'normal' => $fontDir . '\80752832c41f7095bd2b30e5f1653b32',
+  ),
+  'themify' => array(
+    'normal' => $fontDir . '\6e067adf027728099b9d8186e8b227ed',
+  ),
+  'material design icons' => array(
+    'normal' => $fontDir . '\16bf56d13885bc68dcaae5155f8d47d0',
+  ),
+) ?>
